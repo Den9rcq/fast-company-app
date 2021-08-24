9f4926a49eb9343dd6c9bbc607622c2bca5fd984
Denis Bugreev <den9.rcq@gmail.com>
2021-08-24 14:09:01 +0300

    dynamically updating users and deleting them

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
+import api from '../api'
 
 const Users = () => {
+	const [users, setUsers] = useState(api.users.fetchAll());
+	const user = users.map(item => {
+		return (
+			<tr key={item._id}>
+				<td>{item.name}</td>
+				<td>{item.qualities.map(badge => <span key={badge._id} className={`badge me-2 bg-${badge.color}`}>{badge.name}</span>)}</td>
+				<td>{item.profession.name}</td>
+				<td>{item.completedMeetings}</td>
+				<td>{item.rate} \ 5</td>
+				<td><button onClick={() => handleDelete(item._id)} type='button' className='btn btn-danger'>Удалить</button></td>
+			</tr>
+		)
+	})
+	const handleDelete = (userId) => setUsers(users.filter(user => user._id !== userId))
 	return (
 		<div className='wrapper'>
 			<table className="table align-middle">
@@ -15,16 +30,7 @@ const Users = () => {
 				</tr>
 				</thead>
 				<tbody>
-				<tr>
-					<td>Jhon Connor</td>
-					<td>
-						<span className="badge bg-primary me-2">Primary</span>
-					</td>
-					<td>Доктор</td>
-					<td>20</td>
-					<td>2.5</td>
-					<td><button type='button' className='btn btn-danger'>Удалить</button></td>
-				</tr>
+					{user}
 				</tbody>
 			</table>
 		</div>
