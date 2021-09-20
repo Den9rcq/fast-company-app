fda372af3c90ee682958c81eb52f674cc24535c6
Denis Bugreev <den9.rcq@gmail.com>
2021-09-20 14:17:11 +0300

    refactoring Bookmark component

--- src/components/bookMark.jsx
+++ src/components/bookMark.jsx
@@ -2,18 +2,17 @@ import React from "react";
 import PropTypes from "prop-types";
 import { iconFavoritesTrue, iconFavoritesFalse } from "../utils/icons";
 
-const BookMark = ({ favorites, onToggleMark, id }) => {
+const BookMark = ({ favorites, onToggleMark }) => {
     const getFavorites = () =>
         favorites ? iconFavoritesTrue : iconFavoritesFalse;
     return (
-        <span role="button" onClick={() => onToggleMark(id)}>
+        <span role="button" onClick={onToggleMark}>
             {getFavorites()}
         </span>
     );
 };
 BookMark.propTypes = {
     favorites: PropTypes.bool.isRequired,
-    onToggleMark: PropTypes.func.isRequired,
-    id: PropTypes.string.isRequired
+    onToggleMark: PropTypes.func.isRequired
 };
 export default BookMark;
--- src/components/usersTable.jsx
+++ src/components/usersTable.jsx
@@ -2,15 +2,35 @@ import React from "react";
 import PropTypes from "prop-types";
 import TableHeader from "./tableHeader";
 import TableBody from "./tableBody";
-const UsersTable = ({ users, onSort, selectedSort, ...rest }) => {
+import BookMark from "./bookMark";
+
+const UsersTable = ({ users, onSort, selectedSort, onToggleMark, onDelete }) => {
     const columns = {
         name: { path: "name", name: "Имя" },
         qualities: { name: "Профессия" },
         profession: { path: "profession.name", name: "Профессия" },
         completedMeetings: { path: "completedMeetings", name: "Встретился раз" },
         rate: { path: "rate", name: "Оценка" },
-        favorites: { path: "favorites", name: "Избранное" },
-        delete: {}
+        favorites: {
+            path: "favorites",
+            name: "Избранное",
+            component: (user) => (
+                <BookMark
+                    onToggleMark={() => onToggleMark(user._id)}
+                    favorites={user.favorites}/>
+            )
+        },
+        delete: {
+            component: (user) => (
+                <button
+                    onClick={() => onDelete(user._id)}
+                    type="button"
+                    className="btn btn-danger"
+                >
+                    Удалить
+                </button>
+            )
+        }
     };
     return (
         <table className="table align-middle">
@@ -22,6 +42,8 @@ const UsersTable = ({ users, onSort, selectedSort, ...rest }) => {
 UsersTable.propTypes = {
     users: PropTypes.array.isRequired,
     onSort: PropTypes.func.isRequired,
-    selectedSort: PropTypes.object.isRequired
+    selectedSort: PropTypes.object.isRequired,
+    onToggleMark: PropTypes.func.isRequired,
+    onDelete: PropTypes.func.isRequired
 };
 export default UsersTable;
