8c4a5beb0ea09e661dbe6583e2467fffd9f71645
Denis Bugreev <den9.rcq@gmail.com>
2021-10-10 09:53:33 +0300

    validate input & button

---
+++ src/components/textField.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const TextField = ({ label, type, name, value, onChange, errors }) => {
+    return (
+        <div>
+            <label htmlFor={name}>{label}</label>
+            <input
+                type={type}
+                id={name}
+                name={name}
+                value={value}
+                onChange={onChange}
+            />
+            {errors && <p>{errors}</p>}
+        </div>
+    );
+};
+
+TextField.defaultProps = {
+    type: "text"
+};
+
+TextField.propTypes = {
+    label: PropTypes.string,
+    type: PropTypes.string,
+    name: PropTypes.string,
+    value: PropTypes.string,
+    onChange: PropTypes.func,
+    errors: PropTypes.string
+};
+
+export default TextField;
