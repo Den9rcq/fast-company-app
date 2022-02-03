68c043b5eef5cd1c7c3ac529ac72bb87a5a6d73f
Denis Bugreev <den9.rcq@gmail.com>
2022-02-03 10:00:13 +0300

    setting up a working environment

--- server/app.js
+++ server/app.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const confin = require('config')
+const config = require('config')
 const chalk = require('chalk')
 
 const app = express()
 
-const PORT = confin.get('port') ?? 8080
+const PORT = config.get('port') ?? 8080
+
+// if (process.env.NODE_ENV === 'production') {
+//     console.log('Production')
+// } else {
+//     console.log('Developments')
+// }
 
 app.listen(PORT, () => {
     console.log(chalk.green(`Server has been started on port: ${PORT}`))
