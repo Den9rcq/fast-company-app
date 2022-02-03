c5857267f1266c738c2079f91626a20da638076f
Denis Bugreev <den9.rcq@gmail.com>
2022-02-03 11:46:11 +0300

    сreating models

---
+++ server/models/User.js
@@ -0,0 +1,17 @@
+const {Schema, model} = require('mongoose')
+
+const schema = new Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+    completedMeetings: Number,
+    image: String,
+    rate: Number,
+    sex: {type: String, enum: ['male', 'female', 'other']},
+    profession: {type: Schema.Types.ObjectId, ref: 'Profession'},
+    qualities: [{type: Schema.Types.ObjectId, ref: 'Quality'}]
+}, {
+    timestamps: true
+})
+
+module.exports = model('User', schema)
