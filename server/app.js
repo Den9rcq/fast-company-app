cb4cf9e6547316ca279556d95c2d3d9f8c99dd37
Denis Bugreev <den9.rcq@gmail.com>
2022-02-26 17:25:40 +0300

    docker

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const config = require('config')
 const chalk = require('chalk')
+const path = require('path')
 const initDatabase = require('./startUp/initDatabase')
 const routes = require('./routes')
 const cors = require('cors')
@@ -16,11 +17,13 @@ app.use('/api', routes)
 
 const PORT = config.get('port') ?? 8080
 
-// if (process.env.NODE_ENV === 'production') {
-//     console.log('Production')
-// } else {
-//     console.log('Developments')
-// }
+if (process.env.NODE_ENV === 'production') {
+    app.use('/', express.static(path.join(__dirname, 'client')))
+    const indexPath = path.join(__dirname, 'client', 'index.html')
+    app.get('*', (req, res) => {
+        res.sendFile(indexPath)
+    })
+}
 
 async function start() {
     try {
